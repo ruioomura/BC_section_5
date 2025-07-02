8fbea19998219e4eadbca3fbe56654169fd10c55
RuiOomura <asxnr2525@gmail.com>
2025-07-03 00:05:51 +0900

    update:routerDirへ分割

--- api-server/src/app.ts
+++ api-server/src/app.ts
@@ -1,44 +1,16 @@
 import express from 'express';
 import userRouter from './router/user';
-import { PrismaClient } from '@prisma/client';
 import dotenv from 'dotenv';
 
 dotenv.config(); // .envの読み込み
 
 const app = express();
-const prisma = new PrismaClient();
-const port = process.env.PORT || 4000;
-const dbUrl = process.env.DATABASE_URL;
-console.log("接続先DB：", process.env.DATABASE_URL);
-
 app.use(express.json());
 
-// GET /usersでユーザー一覧取得
-// TODO:async調べる
-app.get('/users', async(req, res) => {
-  try {
-    const users = await prisma.user.findMany();
-    res.json(users);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'ユーザー一覧の取得に失敗しました'});
-  }
-});
+// /usersエンドポイントでuserRouter使用
+// TODO:userRouterおさらい
+app.use('/users', userRouter);
 
-// POST /usersでユーザー作成
-app.post("/users", async(req, res) => {
-  const { name, email } = req.body;
-
-  try {
-    const newUser = prisma.user.create({
-      data: { name, email },
-    });
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(400).json({ error: 'ユーザーの作成に失敗しました'});
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(4000, () => {
+  console.log(`Server running on http://localhost:4000`);
 });
--- api-server/src/router/user.ts
+++ api-server/src/router/user.ts
@@ -1,9 +1,38 @@
 
 import { Router } from "express";
-const router = Router();
+import{ PrismaClient } from '@prisma/client';
 
-router.get("/", (req, res) => {
-  res.send("User router works!");
+// TODO:new PrismaClient深堀
+const router = Router();
+const prisma = new PrismaClient();
+
+// TODO:ここでdbUrl定義する意味調べる
+const dbUrl = process.env.DATABASE_URL;
+console.log("接続先DB：", process.env.DATABASE_URL);
+
+// GET /users移行
+// TODO:router以下コードとasync深堀
+router.get("/", async(req, res) => {
+    try {
+        const users = await prisma.user.findMany();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ error: 'ユーザー一覧の取得に失敗しました'});
+    }
+});
+
+// Post /users移行
+router.post('/', async(req, res)=> {
+    const { name, email } = req.body;
+
+    try {
+        const newUser = await prisma.user.create({
+            data: { name, email },
+        });
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(400).json({ error: 'ユーザーの作成に失敗しました'});
+    }
 });
 
 export default router;
